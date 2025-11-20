8277d93f1ff0bce217893c768106f1fd13c45612
SargamG <166918658+SargamG@users.noreply.github.com>
2025-11-20 11:27:17 +0530

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -20,38 +20,39 @@ class GatedConv2d(nn.Module):
         return out
 
 class CNN(nn.Module):
-  def __init__(self):
-    self.project1 = nn.Sequential(nn.Conv2d(259, 256, kernel_size=1, stride=1, padding=0),
-                                  nn.BatchNorm2d(256),
-                                  nn.ELU())
-    self.project2 = nn.Sequential(nn.Conv2d(256, 128, kernel_size=1, stride=1, padding=0),
-                                  nn.BatchNorm2d(128),
-                                  nn.ELU())
-    self.project3 = nn.Sequential(nn.Conv2d(128, 64, kernel_size=1, stride=1, padding=0),
-                                  nn.BatchNorm2d(64),
-                                  nn.ELU())
-    self.conv1 = GatedConv2d(256, 128, kernel_size=7, stride=1, padding=3)
-    self.conv2 = GatedConv2d(128, 64, kernel_size=5, stride=1, padding=2)
-    self.conv3 = GatedConv2d(64, 32, kernel_size=3, stride=1, padding=1)
-    self.conv4 = nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1)
+    def __init__(self):
+        super().__init__()
+        self.project1 = nn.Sequential(nn.Conv2d(259, 256, kernel_size=1, stride=1, padding=0),
+                                      nn.BatchNorm2d(256),
+                                      nn.ELU())
+        self.project2 = nn.Sequential(nn.Conv2d(256, 128, kernel_size=1, stride=1, padding=0),
+                                      nn.BatchNorm2d(128),
+                                      nn.ELU())
+        self.project3 = nn.Sequential(nn.Conv2d(128, 64, kernel_size=1, stride=1, padding=0),
+                                      nn.BatchNorm2d(64),
+                                      nn.ELU())
+        self.conv1 = GatedConv2d(256, 128, kernel_size=7, stride=1, padding=3)
+        self.conv2 = GatedConv2d(128, 64, kernel_size=5, stride=1, padding=2)
+        self.conv3 = GatedConv2d(64, 32, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1)
 
-  def forward(self, x, ae_ft):
-    H, W = x.shape[2], x.shape[3]
-    ae_ft1 = F.interpolate(ae_ft[0], size=(H, W), mode='bilinear', align_corners=False)
-    x = torch.cat([x, ae_ft1], dim=1)
-    x1 = self.project1(x)
-    ft1 = self.conv1(x1)
-
-    ae_ft2 = F.interpolate(ae_ft[1], size=(H, W), mode='bilinear', align_corners=False)
-    ft1 = torch.cat([ft1, ae_ft2], dim=1)
-    ft1 = self.project2(ft1)
-    ft2 = self.conv2(ft1)
-
-    ae_ft3 = F.interpolate(ae_ft[2], size=(H, W), mode='bilinear', align_corners=False)
-    ft2 = torch.cat([ft2, ae_ft3], dim=1)
-    ft2 = self.project3(ft2)
-    ft3 = self.conv3(ft2)
-
-    out = self.conv4(ft3)
-    return out
+    def forward(self, x, ae_ft):
+        H, W = x.shape[2], x.shape[3]
+        ae_ft1 = F.interpolate(ae_ft[0], size=(H, W), mode='bilinear', align_corners=False)
+        x = torch.cat([x, ae_ft1], dim=1)
+        x1 = self.project1(x)
+        ft1 = self.conv1(x1)
+    
+        ae_ft2 = F.interpolate(ae_ft[1], size=(H, W), mode='bilinear', align_corners=False)
+        ft1 = torch.cat([ft1, ae_ft2], dim=1)
+        ft1 = self.project2(ft1)
+        ft2 = self.conv2(ft1)
+    
+        ae_ft3 = F.interpolate(ae_ft[2], size=(H, W), mode='bilinear', align_corners=False)
+        ft2 = torch.cat([ft2, ae_ft3], dim=1)
+        ft2 = self.project3(ft2)
+        ft3 = self.conv3(ft2)
+    
+        out = self.conv4(ft3)
+        return out
     
